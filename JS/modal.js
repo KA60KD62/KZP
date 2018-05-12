4a2ce1079e3eda82783986b6e52397b1f85bcc50
Nova7Dawn <30739009+Nova7Dawn@users.noreply.github.com>
2018-05-13 02:31:22 +0900

    Add files via upload

---
+++ JS/modal.js
@@ -0,0 +1,12 @@
+
+$(function() { 
+    
+  $('.header-menu').click(function() {
+    $('.menu').fadeIn();
+  });
+    
+  $('.close-menu').click(function() {
+    $('.menu').fadeOut();
+  });    
+    
+});
